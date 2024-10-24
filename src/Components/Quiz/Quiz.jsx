e0cb773db36f643f47231265badf37b0c0b686a4
shamsrehmanpro <shamsrehmanpro@gmail.com>
2024-10-24 08:48:40 +0500

    completed

--- src/Components/Quiz/Quiz.jsx
+++ src/Components/Quiz/Quiz.jsx
@@ -1,32 +1,80 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import './Quiz.css'
 import { data } from '../../assets/data'
 
 const Quiz = () => {
-    let [index, setIndex] = useState(2)
+    let [index, setIndex] = useState(0)
     let [questions, setQuestions] = useState(data[index])
+    let [lock, setLock] = useState(false)
+    let [score, setScore] = useState(0)
+    const [result, setResult] = useState(false)
+
+    let Option1 = useRef(null)
+    let Option2 = useRef(null)
+    let Option3 = useRef(null)
+    let Option4 = useRef(null)
+
+    let option_array = [Option1, Option2, Option3, Option4]
 
     const checkAns = (e, ans) => {
-        if(questions.ans === ans){
-            e.target.classList.add("correct")
+        if(lock === false){
+            if(questions.ans === ans){
+                e.target.classList.add("correct")
+                setLock(true)
+                setScore(prev => prev+1)
+            }
+            else{
+                e.target.classList.add("wrong")
+                setLock(true)
+                option_array[questions.ans-1].current.classList.add("correct")  
+            }
         }
-        else{
-            e.target.classList.add("wrong")
+    }
+
+    const next = () => {
+        if(lock === true) {
+            if (index === data.length-1){
+                setResult(true)
+                return 0
+            }
+            setIndex(++index)
+            setQuestions(data[index])
+            setLock(false)
+            option_array.map((option)=> {
+                option.current.classList.remove("correct")
+                option.current.classList.remove("wrong")
+                return null
+            })
         }
+    }
+
+    const reset = () => {
+        setIndex(0)
+        setQuestions(data[0])
+        setScore(0)
+        setLock(false)
+        setResult(false)
     }
   return (
     <div className='container'>
         <h1>Quiz App</h1>
         <hr />
-        <h2>{index +1}. {questions.question} </h2>
-        <ul>
-            <li onClick={(e) => {checkAns(e, 1)}}>{questions.option1}</li>
-            <li onClick={(e) => {checkAns(e, 2)}}>{questions.option2}</li>
-            <li onClick={(e) => {checkAns(e, 3)}}>{questions.option3}</li>
-            <li onClick={(e) => {checkAns(e, 4)}}>{questions.option4}</li>
-        </ul>
-        <button >Next</button>
-        <div className="index">1 of 5 questions</div>
+
+        {
+            result ? <></> : <> <h2>{index +1}. {questions.question} </h2>
+            <ul>
+                <li ref={Option1} onClick={(e) => {checkAns(e, 1)}}>{questions.option1}</li>
+                <li ref={Option2} onClick={(e) => {checkAns(e, 2)}}>{questions.option2}</li>
+                <li ref={Option3} onClick={(e) => {checkAns(e, 3)}}>{questions.option3}</li>
+                <li ref={Option4} onClick={(e) => {checkAns(e, 4)}}>{questions.option4}</li>
+            </ul>
+            <button onClick={next} >Next</button>
+            <div className="index">{index+1} of {data.length} questions</div></>
+        }
+
+        {result ? <><h2>You Scored {score} out of {data.length}</h2>
+        <button onClick={reset}>Reset</button></>: <></>}
+       
     </div>
   )
 }
